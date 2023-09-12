c2f45fbd9558683cbba248c30afdfdc8f8b2802c
Adil Prawirdani <prawirdani@icloud.com>
2023-09-13 06:28:44 +0700

    Add config

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server ServerConfig
+}
+
+type ServerConfig struct {
+	Port         string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	Env          string
+}
+
+type Cors struct {
+	Credentials bool
+}
+
+func LoadConfig() (*Config, error) {
+	v := viper.New()
+
+	v.AddConfigPath("./config/")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+	var c Config
+
+	if err := v.Unmarshal(&c); err != nil {
+		log.Panicf("failed decode config into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
