1bb81c44b441571b8836d5e575d3729c5fc09eb2
Prawirdani <prawirdani@gmail.com>
2023-09-18 01:45:15 +0700

    add IP Forwarder on request logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -14,12 +14,12 @@ func InitLogger() {
 }
 
 type requestLogAttributes struct {
-	Method     string
-	Uri        string
-	Address    string
-	StatusCode int
-	StatusText string
-	TimeTaken  time.Duration
+	Method      string
+	Uri         string
+	ForwardedIP string
+	StatusCode  int
+	StatusText  string
+	TimeTaken   time.Duration
 }
 
 type resRecorder struct {
@@ -38,7 +38,7 @@ func HttpRequestLogger(rl requestLogAttributes) {
 		"HTTP Request Log",
 		slog.String("method", rl.Method),
 		slog.String("url", rl.Uri),
-		slog.String("from", rl.Address),
+		slog.String("from", rl.ForwardedIP),
 		slog.Int("status_code", rl.StatusCode),
 		slog.String("status_text", rl.StatusText),
 		slog.Float64("time_taken(ms)", float64(rl.TimeTaken.Microseconds())/float64(1000)),
@@ -55,15 +55,18 @@ func RequestLogger(next http.Handler) http.Handler {
 			Status:         http.StatusOK,
 		}
 		next.ServeHTTP(rec, r)
+
+		// Retrieve forwarded client ip from reverse-proxy
+		forwardedIP := r.Header.Get("X-Real-IP")
 		duration := time.Since(start)
 
 		logAttributes := &requestLogAttributes{
-			Method:     r.Method,
-			Uri:        r.RequestURI,
-			Address:    r.RemoteAddr,
-			StatusCode: rec.Status,
-			StatusText: http.StatusText(rec.Status),
-			TimeTaken:  duration,
+			Method:      r.Method,
+			Uri:         r.RequestURI,
+			ForwardedIP: forwardedIP,
+			StatusCode:  rec.Status,
+			StatusText:  http.StatusText(rec.Status),
+			TimeTaken:   duration,
 		}
 
 		HttpRequestLogger(*logAttributes)
