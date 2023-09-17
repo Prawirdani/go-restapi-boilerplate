1b1896772fe8207ff37d12e48b74e322d47989ef
Adil Prawirdani <prawirdani@icloud.com>
2023-09-17 21:52:03 +0700

    add initschema flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,59 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log/slog"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/prawirdani/go-restapi-boilerplate/app"
 	"github.com/prawirdani/go-restapi-boilerplate/config"
+	"github.com/prawirdani/go-restapi-boilerplate/db"
 	_ "github.com/prawirdani/go-restapi-boilerplate/docs"
-	"github.com/prawirdani/go-restapi-boilerplate/internal/index"
-	"github.com/prawirdani/go-restapi-boilerplate/pkg/utils"
-	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/prawirdani/go-restapi-boilerplate/internal/user"
+	"github.com/prawirdani/go-restapi-boilerplate/pkg/logger"
 )
 
-// @title			Swagger Docs (API TITLE HERE)
-// @version		1.0
-// @description	This is an api Swagger.
-// @BasePath		/v1
+func init() {
+	logger.InitLogger()
+}
+
+//	@title			Swagger Docs (RESTAPI BoilerPlate)
+//	@version		1.0
+//	@description	This is an api Swagger.
+//	@BasePath		/v1
 func main() {
 	conf, err := config.LoadConfig()
-	utils.PanicIfErr(err)
+	if err != nil {
+		slog.Error("Load config error", "error", err)
+	}
+
+	pgDB := db.NewPostgreSQL(*conf)
+	defer pgDB.Close(context.Background())
+
+	var initSchema bool
+	flag.BoolVar(&initSchema, "initschema", false, "Initialize the schema")
+	flag.Parse()
+	if initSchema {
+		db.InitSchema(pgDB)
+	}
 
 	mainRouter := app.NewMainRouter(conf)
-	mainRouter.Get("/swagger/*", httpSwagger.WrapHandler)
+	appRouter := app.NewSubRouter()
 
-	appRouter := chi.NewRouter()
+	userRepository := user.NewUserRepository(pgDB)
+	userService := user.NewUserService(userRepository)
+	userHandler := user.NewUserHandler(userService)
 
-	indexHandler := index.NewIndexHandler()
 	appRouter.Route("/v1", func(r chi.Router) {
-		indexHandler.Routes(r)
+		userHandler.Routes(r)
 	})
 
 	mainRouter.Mount("/", appRouter)
 
 	svr := app.NewServer(conf, mainRouter)
 	svr.Start()
+}
+
+func someHandler(c chi.Context) {
 }
