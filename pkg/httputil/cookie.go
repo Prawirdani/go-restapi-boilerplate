d68c8ac7b08a2fa4d9285f475d466c091ffecf5b
Prawirdani <prawirdani@gmail.com>
2023-09-22 23:56:06 +0700

    add cookies utility

---
+++ pkg/httputil/cookie.go
@@ -0,0 +1,38 @@
+package httputil
+
+import (
+	"net/http"
+	"time"
+)
+
+const (
+	REFRESH_TOKEN_COOKIE_NAME = "refreshToken"
+	ACCESS_TOKEN_COOKIE_NAME  = "accessToken"
+)
+
+func GetCookieValue(r *http.Request, tokenName string) string {
+	if cookie, err := r.Cookie(tokenName); err == nil {
+		return cookie.Value
+	}
+	return ""
+}
+
+func SetCookies(name string, value string, expires time.Time, w http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     "/",
+		Expires:  expires,
+		HttpOnly: true,
+	}
+
+	http.SetCookie(w, cookie)
+}
+
+func SetCookieAccessToken(tokenValue string, rw http.ResponseWriter) {
+	SetCookies(ACCESS_TOKEN_COOKIE_NAME, tokenValue, time.Now().Add(1*time.Minute), rw)
+}
+
+func SetCookieRefreshToken(tokenValue string, rw http.ResponseWriter) {
+	SetCookies(REFRESH_TOKEN_COOKIE_NAME, tokenValue, time.Now().Add(1*time.Hour), rw)
+}
