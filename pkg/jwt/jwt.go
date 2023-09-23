db7899253653b348173e9af962a36c481cc97c2e
Prawirdani <prawirdani@gmail.com>
2023-09-23 23:12:14 +0700

    refactor jwt utility

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,8 +1,8 @@
 package jwt
 
 import (
-	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -13,97 +13,132 @@ import (
 
 var jwtSecret = []byte("akBfd4k+enMwQ61edGpfsu3uLvxXa9aIlM0MIGm6BobvIGA/r3xUY0CqCyGpl65cp8ytxr1gg8Ssp9SEmDOEGQ==")
 
+type TokenType int
+
+const (
+	// Type for refresh token
+	REFRESH_TOKEN TokenType = iota
+
+	// Type for access token
+	ACCESS_TOKEN
+)
+
+const (
+	// Constant key for extract id value from JWT Map Claims
+	CLAIMS_KEY_ID = "id"
+	// Constant key for extract username value from JWT Map Claims
+	CLAIMS_KEY_USERNAME = "username"
+)
+
+// JWT Payload Claims
 type Claims struct {
 	Id       string `json:"id,omitempty"`
 	Username string `json:"username,omitempty"`
 	jwt.RegisteredClaims
 }
 
-type TokenPairs struct {
-	AccessToken  string `json:"access_token,omitempty"`
-	RefreshToken string `json:"refresh_token,omitempty"`
+type Token struct {
+	Type  TokenType
+	Value string
 }
 
-func (tp *TokenPairs) SetToCookies(rw http.ResponseWriter) {
-	httputil.SetCookieAccessToken(tp.AccessToken, rw)
-	httputil.SetCookieRefreshToken(tp.RefreshToken, rw)
+type TokenPair struct {
+	RefreshToken *Token
+	AccessToken  *Token
 }
 
-// Todo it should contains UUID Store to Redis. When refresh new access token it will look up the redis with this UUID and return the payload as long as the refresh token is not yet expired.
-/* Signing Refresh Token takes User Id as payload */
-func SignRefreshToken(userID string) string {
+// Sign Token as Refresh Token
+func (t *Token) SignAsRefreshToken(user user.User) error {
+	t.Type = REFRESH_TOKEN
+	tokenString, err := generateToken(user, REFRESH_TOKEN)
+	if err == nil {
+		t.Value = tokenString
+	}
+	return err
+}
+
+// Sign Token as Access Token
+func (t *Token) SignAsAccessToken(user user.User) error {
+	t.Type = ACCESS_TOKEN
+	tokenString, err := generateToken(user, ACCESS_TOKEN)
+	if err == nil {
+		t.Value = tokenString
+	}
+	return err
+}
+
+// Set token value into cookie
+func (t *Token) SetToCookie(w http.ResponseWriter) {
+	timeNow := time.Now()
+	switch t.Type {
+	case ACCESS_TOKEN:
+		httputil.SetCookies(httputil.ACT_COOKIE_NAME, t.Value, timeNow.Add(1*time.Minute), w)
+	default:
+		httputil.SetCookies(httputil.RFT_COOKIE_NAME, t.Value, timeNow.Add(60*time.Minute), w)
+	}
+}
+
+// Sign a pair of jwt tokens.
+func (tp *TokenPair) SignPair(user user.User) error {
+	tp.RefreshToken = new(Token)
+	tp.AccessToken = new(Token)
+
+	if err := tp.RefreshToken.SignAsRefreshToken(user); err != nil {
+		return err
+	}
+	if err := tp.AccessToken.SignAsAccessToken(user); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Sign the values of both tokens to cookies
+func (tp *TokenPair) SetToCookies(w http.ResponseWriter) {
+	tp.RefreshToken.SetToCookie(w)
+	tp.AccessToken.SetToCookie(w)
+}
+
+// Token generator, based on user data
+func generateToken(u user.User, tokenType TokenType) (string, error) {
+	timeNow := time.Now()
 	claims := &Claims{
-		Id: userID,
+		Id: u.Id,
 		RegisteredClaims: jwt.RegisteredClaims{
-			Issuer:    "rfToken",
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 1)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			IssuedAt: jwt.NewNumericDate(timeNow),
 		},
 	}
-	
+	switch {
+	case tokenType == REFRESH_TOKEN:
+		claims.ExpiresAt = jwt.NewNumericDate(timeNow.Add(60 * time.Minute))
+	case tokenType == ACCESS_TOKEN:
+		claims.Username = u.Username
+		claims.ExpiresAt = jwt.NewNumericDate(timeNow.Add(30 * time.Second))
+	}
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	sign, err := token.SignedString(jwtSecret)
 	if err != nil {
-		panic(err)
+		slog.Error("JWT Signing error", slog.Any("cause", err))
+		return "", err
 	}
-	return sign
+	return sign, nil
 }
 
-/* Signing Access Token takes User Id and Username  as payload */
-func SignAccessToken(user *user.User) string {
-	claims := &Claims{
-		Id:       user.Id,
-		Username: user.Username,
-		RegisteredClaims: jwt.RegisteredClaims{
-			Issuer:    "acToken",
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Second * 30)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-		},
-	}
-	
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	sign, err := token.SignedString(jwtSecret)
-	if err != nil {
-		panic(err)
-	}
-	return sign
-}
-
-/* Sign Access and Refresh token at the same time, used for login */
-func SignPairs(u *user.User) *TokenPairs {
-	tokenPairs := &TokenPairs{
-		RefreshToken: SignRefreshToken(u.Id),
-		AccessToken:  SignAccessToken(u),
-	}
-
-	return tokenPairs
-}
-
-
-/* Returning Token Map Claims straighly from incoming request */
+// Validate token and return map claims
 func ValidateFromRequest(r *http.Request, tokenCookieName string) (map[string]interface{}, error) {
 	tokenString := httputil.GetCookieValue(r, tokenCookieName)
-	if tokenString == "" {
-		return nil, httputil.ErrUnauthorized("please login to proceed!")
-	}
 
 	claims := jwt.MapClaims{}
 
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			return nil, httputil.ErrInternalServer(fmt.Errorf("unexpected signing method: %v", t.Header["alg"]))
 		}
 		return jwtSecret, nil
 	})
-	if err != nil {
-		if errors.Is(err, jwt.ErrSignatureInvalid) {
-			return nil, httputil.ErrUnauthorized("invalid token signature")
-		}
-		return nil, err
-	}
-
-	if !token.Valid {
-		return nil, httputil.ErrUnauthorized("invalid/expired token")
+	if err != nil || !token.Valid {
+		return nil, httputil.ErrUnauthorized("invalid or expired token")
 	}
 
 	return claims, nil
